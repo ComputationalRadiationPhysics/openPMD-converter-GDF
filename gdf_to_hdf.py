be6135402d2515f1995b2f61cc3f73663221a63d
Unknown <Bastrakova.Kseinia@gmail.com>
2018-06-11 14:13:00 +0200

    Add writing version of creator and gdf version

--- gdf_to_hdf.py
+++ gdf_to_hdf.py
@@ -1,9 +1,9 @@
 
 import h5py
-
 import struct
 import os
 import sys
+import time
 
 def gdf_to_hdf(gdf_file_directory, hdf_file_directory):
     print ('Converting .gdf to .hdf file with hierical layout.')       
@@ -13,26 +13,48 @@ def gdf_to_hdf(gdf_file_directory, hdf_file_directory):
 	
 	#Constants
     GDFID  = 94325877;
+    GDFNAMELEN = 16;
 
     with open(gdf_file_directory, 'rb') as f:
         
         gdf_id_check = struct.unpack('i', f.read(4))[0]
         if gdf_id_check != GDFID:
             raise RuntimeWarning('File directory is not a .gdf file')
-        
-        
-		
-    f.close()		
+        time_created = struct.unpack('i', f.read(4))[0]
+        hdf_f.attrs['time_created'] = str(time_created) + ': ' + str(time.ctime(int(time_created)))
+
+        # get creator name and use string part upto zero-character
+        creator = list(f.read(GDFNAMELEN))
+        print(type(f.read(GDFNAMELEN)))
+        dest = f.read(GDFNAMELEN)
+
+        major = struct.unpack('B', f.read(1))[0]
+        minor = struct.unpack('B', f.read(1))[0]
+        hdf_f.attrs['gdf_version'] = str(major) + '.' + str(minor)
+
+        major = struct.unpack('B', f.read(1))[0]
+        minor = struct.unpack('B', f.read(1))[0]
+        hdf_f.attrs['creator_version'] = str(major) + '.' + str(minor)
+
+        data_group = hdf_f.create_group('data')
+
+        # Initialise values to print progress to terminal
+        file_size = os.stat(gdf_file_directory)[6]
+        start_time = time.time()
+        last_running_time = 0
+
+
+        f.seek(2, 1)  # skip to next block
+
+    f.close()
     hdf_f.close()
     print ('Converting .gdf to .hdf file with hierical layout... Complete.')
-    
     
 def terminal_call(terminal_args):
     hierical_suffix = '_hierical'
     
     gdf_arg = False
     hdf_arg = False
-    keep_hierical = False
 
     #
     for arg in terminal_args:
@@ -42,8 +64,6 @@ def terminal_call(terminal_args):
         elif arg[-4:] == '.hdf':
             hdf_file_directory = arg
             hdf_arg = True
-        elif arg in ['True', 'true', 'TRUE', 't', 'T', 'Yes', 'yes', 'YES', 'y', 'Y']:
-            keep_hierical = True
 
     if gdf_arg:
         if os.path.exists(gdf_file_directory):
@@ -55,8 +75,6 @@ def terminal_call(terminal_args):
                 hdf_slab_file_directory = hdf_file_directory
                 hdf_file_directory = hdf_slab_file_directory[:-4] + hierical_suffix + '.hdf'
             gdf_to_hdf(gdf_file_directory, hdf_file_directory)
-            if not keep_hierical:
-                print ('Removing .hdf with hierical layout.')
         else:
             print ('The .gdf file does not exist to convert to .hdf')
 
