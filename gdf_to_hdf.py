6ee55c857ad3b24e12631a5f5b22079f245e481b
Unknown <Bastrakova.Kseinia@gmail.com>
2018-07-16 11:23:58 +0200

    Change size of date attribute

--- gdf_to_hdf.py
+++ gdf_to_hdf.py
@@ -50,7 +50,7 @@ def add_creation_time(gdf_file, hdf_file):
     time_created = struct.unpack('i', gdf_file.read(4))[0]
     format_time = datetime.datetime.fromtimestamp(time_created)
     format_time = format_time.strftime("%Y-%m-%d %H:%M:%S %Z")
-    hdf_file.attrs['date'] = format_time
+    hdf_file.attrs.create('openPMD', format_time, None, dtype='<S11')
 
 
     add_creation_time(gdf_file, hdf_file)
