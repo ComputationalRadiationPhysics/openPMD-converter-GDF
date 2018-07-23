0807b83e22ffeec8d918fa6b698730fc336af247
Unknown <Bastrakova.Kseinia@gmail.com>
2018-07-23 15:02:23 +0200

    Add unitSI, timeOffset, unitDimention attributes to id dataset

--- gdf_to_hdf.py
+++ gdf_to_hdf.py
@@ -242,7 +242,13 @@ def name_to_group(name, particles, size, gdf_file):
             add_group_attributes(gdf_file, particles, name_atribute, size)
     elif name[0:2] == 'ID':
             value = fromfile(gdf_file, dtype=dtype('f8'), count=int(size / 8))
-            particles.create_dataset('ID', data=value)
+            particles.create_dataset('id', data=value)
+            attribute_dataset = particles.require_dataset('id', value.shape, dtype=dtype('f8'))
+            attribute_dataset.attrs.create('unitSI', 1.0, None, dtype=np.dtype('float'))
+            attribute_dataset.attrs.create('timeOffset', 0.0, None, dtype=np.dtype('float'))
+            attribute_dataset.attrs.create('unitDimension',
+                                           Elements.dict_dimensions.get('id'), None,
+                                           dtype=np.dtype('float'))
     elif name[0:4] == 'time':
         value = fromfile(gdf_file, dtype=dtype('f8'), count=int(size / 8))
         particles.create_dataset('time', data=value)
