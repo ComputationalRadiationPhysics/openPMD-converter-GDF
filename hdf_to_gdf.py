b07d8defa188330ae47ee250de8c2c9e8b8a9fa5
Unknown <Bastrakova.Kseinia@gmail.com>
2018-07-24 09:43:35 +0200

    Add converter function

--- hdf_to_gdf.py
+++ hdf_to_gdf.py
@@ -1,3 +1,16 @@
+def converter(hdf_file, gdf_file):
+    if hdf_file != '':
+        if os.path.exists(hdf_file):
+            if gdf_file == '':
+                gdf_file = hdf_file[:-4] + '.gdf'
+                print('Destination .gdf directory not specified. Defaulting to ' + gdf_file)
+            else:
+                gdf_file = gdf_file[:-4] + '.hdf'
+            hdf_to_gdf(hdf_file, gdf_file)
+        else:
+            print('The .hdf file does not exist to convert to .gdf')
+
+
 def main(file_names):
     gdf_path, hdf_path = files_from_args(file_names)
     converter(hdf_path, gdf_path)
