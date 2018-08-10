6cc9ec42dbfe340d7f363a3b7ae0a3655d70c350
Unknown <Bastrakova.Kseinia@gmail.com>
2018-08-10 13:28:13 +0200

    Add write_ascii_name function

--- hdf_to_gdf.py
+++ hdf_to_gdf.py
@@ -94,6 +94,15 @@ def write_iteration(hdf_file, gdf_file):
        array = dict_array_names[key]
        if dict_datasets.get(key) != None:
            write_double_dataset(gdf_file, dict_datasets[key], len(array), array)
+def write_ascii_name(name, size, gdf_file, ascii_name):
+    write_string(name, gdf_file)
+    type_bin = struct.pack('i', int(1025))
+    gdf_file.write(type_bin)
+    size_bin = struct.pack('i', int(size))
+    gdf_file.write(size_bin)
+    charlist = list(ascii_name)
+    type_size = str(size) + 's'
+    gdf_file.write(struct.pack(type_size, ascii_name.encode('ascii')))
 
 
 def write_double_dataset(gdf_file, name, size, array_dataset):
