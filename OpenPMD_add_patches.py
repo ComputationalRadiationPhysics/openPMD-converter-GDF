a0d54c96d37796fa85f19b105b9950021333023f
Unknown <Bastrakova.Kseinia@gmail.com>
2018-09-10 11:01:49 +0200

    Add gridSize and devicesNumber arguments to command line

--- OpenPMD_add_patches.py
+++ OpenPMD_add_patches.py
@@ -6,6 +6,13 @@ import h5py
 from shutil import copyfile
 
 
+def OpenPMD_add_patches(hdf_file_name, name_of_file_with_patches, grid_sizes, devices_numbers):
+    copyfile(hdf_file_name, name_of_file_with_patches)
+
+    file_with_patches = h5py.File(name_of_file_with_patches)
+    hdf_file = h5py.File(hdf_file_name)
+
+
 def add_patches(hdf_file, hdf_file_with_patches):
     """ Check correct of arguments"""
 
@@ -30,6 +37,13 @@ if __name__ == "__main__":
                         help="hdf file without patches")
     parser.add_argument("-result", metavar='hdf_file_with_patches', type=str,
                         help="path to result file with patches")
+    parser.add_argument("-gridSize", type=int, nargs=3,
+                        metavar=('size_grid_x', 'size_grid_y', 'size_grid_z'),
+                        help="Size of the simulation grid in cells as x y z")
+    parser.add_argument("-devicesNumber", type=int, nargs=3,
+                        metavar=('devices_number_x', 'devices_number_y', 'size_grid_z'),
+                        help="Number of devices in each dimension (x,y,z)")
+
     args = parser.parse_args()
 
     converter(args.hdf, args.result)
