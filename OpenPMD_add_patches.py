f9f1e5582cf64049a3b76eadc040e38d0231d275
Unknown <Bastrakova.Kseinia@gmail.com>
2018-10-29 10:43:26 +0100

    Fix name in add_patches

--- OpenPMD_add_patches.py
+++ OpenPMD_add_patches.py
@@ -437,15 +437,16 @@ def add_patches(hdf_file, hdf_file_with_patches, grid_sizes, devices_number):
     """ Check correct of arguments"""
 
     name_of_file_with_patches = ''
+    field_size = 0.00001
     if hdf_file != '':
         if os.path.exists(hdf_file):
             name = hdf_file[:-4]
             idx_of_name = name.rfind('/')
             if idx_of_name != -1:
-                name_of_file_with_patches = hdf_file_with_patches + hdf_file[idx_of_name + 1: -3] + 'with_patches.h5'
+                name_of_file_with_patches = hdf_file_with_patches + hdf_file[idx_of_name + 1: -4] + 'with_patches.h5'
             else:
                 name_of_file_with_patches = hdf_file_with_patches + hdf_file[:-3] + '.h5'
-            OpenPMD_add_patches(hdf_file, name_of_file_with_patches, grid_sizes, devices_number)
+            OpenPMD_add_patches(hdf_file, name_of_file_with_patches, grid_sizes, devices_number, field_size)
         else:
             print('The .hdf file does not exist')
 
